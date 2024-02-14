a1b0fe8251f5b21540537f2bf3f728643c07eed7
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2024-02-13 21:55:02 -0500

    Primer archivo creado

---
+++ intro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    
+    std::cout<<"Hello Wold from C++ in VS Code!"<<std::endl;
+    std::cout<<"Increible lo que es tener que usar std porque es una mala práctica usar el namespace";
+    return 0;
+}
